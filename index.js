d753f13b94c1b0afa433a7a8060d4a787ffebc28
Robert Sleppy <rassleppy@gmail.com>
2021-11-17 00:00:06 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let message = []
+    for(let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return message;
+}
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+//   function countDown( startingNumber ) {
+//     while ( startingNumber > 0 ) {
+//       console.log( startingNumber );
+//       startingNumber -= 1;
+//     }
+//     console.log( startingNumber );
+//   }
